650bd6cf13832a0577ea5547a732019fa26dd182
Alex Swiontek <alxcodes@gmail.com>
2018-11-02 15:43:58 -0700

    production URL

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(helmet());
 // Set cors origin
 app.use(
   cors({
-    origin: ['http://localhost:3000']
+    origin: ['http://localhost:3000', 'https://hiking-log.netlify.com']
   })
 );
 
