7ee55b62ba86c831cdda1e4caf7e4225ca0e0cb4
Alex Swiontek <alxcodes@gmail.com>
2018-12-16 17:41:46 -0700

    provide default value

--- handlers/sorting.js
+++ handlers/sorting.js
@@ -1,6 +1,12 @@
 const sorting = type => {
   const [field, value] = type.split('_');
 
+  if (!field || !value) {
+    return {
+      created: 'desc',
+    };
+  }
+
   return { [field]: value };
 };
 
