f6915e807c69071c8bbb7be888a583acd1e46f57
Alex Swiontek <>
2020-04-28 18:34:57 -0700

    idk

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import environmental variables
-require('dotenv').config();
+require("dotenv").config();
 
 // Connect to our Database and handle any bad connections
-mongoose.connect(
-  process.env.DATABASE_URI,
-  { useNewUrlParser: true }
-);
+mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true });
 mongoose.Promise = global.Promise; // Tell Mongoose to use ES6 promises
-mongoose.connection.on('error', err => {
+mongoose.connection.on("error", (err) => {
   console.error(`🚫🚫🚫 → ${err.message}`);
 });
 
 // Import all models
-require('./models/User');
-require('./models/Hike');
+require("./models/User");
+require("./models/Hike");
 
 // Start the app!
-const app = require('./app');
-app.set('port', process.env.PORT || 7777);
-const server = app.listen(app.get('port'), () => {
+const app = require("./app");
+app.set("port", process.env.PORT || 7777);
+const server = app.listen(app.get("port"), () => {
   console.log(`Currently running on http://localhost:${server.address().port}`);
 });
